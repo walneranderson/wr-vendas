54abceaf43bab61cbb91c6b0754aca04c24a2f35
Walner Anderson <walneranderson_sjs@hotmail.com>
2019-09-27 01:18:07 -0300

    system settings 0.0.2

--- application/views/cadastroUsuario.php
+++ application/views/cadastroUsuario.php
@@ -3,10 +3,10 @@
 <head>
   <meta charset="utf-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <meta name="description" content="Sistema de vendas, página de cadastro de usuário - WR Vendas">
+  <meta name="description" content="Sistema de vendas, página de cadastro de usuários - WR Vendas">
   <link rel="apple-touch-icon" sizes="76x76" href="<?php echo base_url('assets/img/apple-icon.png'); ?>">
   <link rel="icon" type="image/png" href="<?php echo base_url('assets/img/favicon.png'); ?>">
-  <title>WR Vendas | Painel Cadatro de Usuário</title>
+  <title>WR Vendas | Painel Cadatro de Usuários</title>
   <link href="<?php echo base_url('assets/css/nucleo-icons.css'); ?>" rel="stylesheet" />
   <link href="<?php echo base_url('assets/css/black-dashboard.css?v=1.0.0'); ?>" rel="stylesheet" />
 </head>
